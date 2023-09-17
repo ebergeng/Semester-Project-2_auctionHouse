acda92276622fdc42f126cf95a510303e20fb7a3
ebergeng <59417456+ebergeng@users.noreply.github.com>
2023-09-17 09:38:04 +0200

    s

--- src/js/eventlisteners/screensizelistener.js
+++
@@ -1,9 +0,0 @@
-export function mobileView() {
-  window.addEventListener("resize", () => {
-    if (window.innerWidth > 991) {
-      return false;
-    } else {
-      return true;
-    }
-  });
-}
